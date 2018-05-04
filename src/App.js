992b780fe806e24ec0c70e4f50b5111190829776
Roman Kontsevoy <1604881@gmail.com>
2018-05-04 19:02:39 +0300

    control-line added

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
 import React, {Component} from 'react';
 import './App.css';
 
-const countOfSquares = 10;
+const countOfSquares = 5;
 const countOfRows = 2;
 
 class Square extends Component {
-    //handleChange(e) {
-    //	this.props.onChange(e.target.value);
-    //}
     render() {
         return (
             <input className="Square"
-				data-square-key={this.props.squareKey}
-				value={this.props.value}
-				onChange={(e) => this.props.onChange(e)}/>
+                   data-square-key={this.props.squareKey}
+                   value={this.props.value}
+                   onChange={(e) => this.props.onChange(e)}
+                   onRange={(e) => this.props.onFocus(e)}
+            />
         );
     };
 }
-;
+
 
 class Row extends Component {
     renderSquare(s, r, arr) {
         const arrOfSquares = [];
         for (let i = 0; i < s; i++) {
-            let squareId = r*countOfSquares+i;
+            let squareId = r * countOfSquares + i;
             let square = <Square key={squareId}
-								squareKey={squareId}
-                                 value={arr[squareId]}
+                                 squareKey={squareId}
+                                 value={arr[squareId].value}
                                  onChange={(e) => this.props.onChange(e)}
+                                 onFocus={(e) => this.props.onFocus(e)}
             />;
             squareId += 1;
             arrOfSquares.splice(arrOfSquares.length, 0, square);
@@ -39,7 +39,7 @@ class Row extends Component {
 
     render() {
         return (
-            <div className="Row" >
+            <div className="Row">
                 {this.renderSquare(countOfSquares, this.props.rowKey, this.props.squares)}
             </div>
         );
@@ -47,30 +47,15 @@ class Row extends Component {
 }
 
 class Table extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            squares: Array(countOfSquares*countOfRows).fill(''),
-        };
-        this.handleChange = this.handleChange.bind(this);
-    }
-
-    handleChange(e) {
-		let squareId = e.target.getAttribute('data-square-key');
-		// function does not know squares number
-        const squares = this.state.squares.slice();
-        squares[squareId] = e.target.value;
-        this.setState({squares: squares});
-    }
-
     renderRow(r) {
         const arrOfRows = [];
-		
+
         for (let i = 0; i < r; i++) {
             let row = <Row key={i}
                            rowKey={i}
-                           squares={this.state.squares}
-                           onChange={(e) => this.handleChange(e)}
+                           squares={this.props.squares}
+                           onChange={(e) => this.props.onChange(e)}
+                           onFocus={(e) => this.props.onFocus(e)}
             />;
             arrOfRows.splice(arrOfRows.length, 0, row);
         }
@@ -81,23 +66,97 @@ class Table extends Component {
 
     render() {
         return (
-            <div className="Table" >
+            <div className="Table"
+                 squares={this.props.squares}
+                 onChange={(e) => this.props.onChange(e)}
+                 onFocus={(e) => this.props.onFocus(e)}
+            >
                 {this.renderRow(countOfRows)}
             </div>
         );
     }
 }
 
-class App extends Component {
+class ControlLine extends Component {
     render() {
+        return (
+            <input className="ControlLine"
+                   placeholder={this.props.placeholder}
+                   value={this.props.value}
+                   onChange={(e) => this.props.onChange(e)}
+                   // onFocus={(e) => this.props.onFocus(e)}
+            />
+        );
+    };
+}
+
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(countOfSquares * countOfRows).fill({
+                value: '',
+            }),
+            focusedSquare: []
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleFocus = this.handleFocus.bind(this);
+
+    }
+
+    handleChange(e) {
+        e.stopPropagation();
+        let squareId = e.target.getAttribute('data-square-key');
+        const squares = this.state.squares.slice();
+        // squares[squareId].value = e.target.value; // назначает value не одному, а всем элементам массива
+        // Так назначает одному, но перезаписывает элемет польностью, а не одно его свойство:
+        squares[squareId] = {
+            value: e.target.value,
+        };
+        this.setState({squares: squares});
+    }
+
+    // handleControlChange(n) {
+    //     e.stopPropagation();
+    //     let squareId = e.target.getAttribute('data-square-key');
+    //     const squares = this.state.squares.slice();
+    //     // squares[squareId].value = e.target.value; // назначает value не одному, а всем элементам массива
+    //     // Так назначает одному, но перезаписывает элемет польностью, а не одно его свойство:
+    //     squares[squareId] = {
+    //         value: e.target.value,
+    //     };
+    //     this.setState({squares: squares});
+    // }
+
+    handleFocus(e) {
+        let squareId = e.target.getAttribute('data-square-key');
+        const focusedSquare = this.state.focusedSquare.slice();
+        focusedSquare.push(squareId);
+        this.setState({focusedSquare: focusedSquare});
+    }
+
+    render() {
+        console.log(this.state.focusedSquare);
+        let focusedSquare = this.state.focusedSquare;
+
         return (
             <div className="App">
                 <header className="App-header">
                     <h1 className="App-title">Welcome to Excel on React</h1>
                 </header>
                 <div className="App-intro">
-					
-                    <Table />
+                    <ControlLine
+                        value={this.state.focusedSquare}
+                        placeholder="Control"
+                        // onChange={this.handleControlChange(this.state.squares[focusedSquare[focusedSquare.length-1]])}
+                    />
+                    <Table
+                        squares={this.state.squares}
+                        onChange={(e) => this.handleChange(e)}
+                        onFocus={(e) => this.handleFocus(e)}
+                    />
 
                 </div>
             </div>
